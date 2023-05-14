f84f49f57d2bfea460f45ebb2c9c62206dd2f88d
IzabelaMatusiewicz <im123022@student.sgh.waw.pl>
2023-05-14 11:26:33 +0200

    augumentation function

--- preprocessing.py
+++ preprocessing.py
@@ -1,19 +1,45 @@
 from PIL import Image
 import os
 
-# Imgage scalling function
-
 # Open the image file
 image = Image.open("H:\Python\Repozytoria\Team_work_trees_clasification\df\Original\Beech\IMG20211213122853.jpg")
 # Get the resolution (width and height) of the image
 width, height = image.size
 print(f"Image resolution: {width}x{height}")
 target_resolution = 512
-rescale_value = (target_resolution / width) * 100
+rescale_value = 100 - (target_resolution / width)
 print(f"Rescale value: {rescale_value}")
 width = int(image.size[1] * rescale_value / 100)
 height = int(image.size[0] * rescale_value / 100)
 print(f"Image resolution: {width}x{height}")
+
+
+
+# Augumentation
+from keras.preprocessing.image import ImageDataGenerator
+
+
+def augment_data(x_train):
+    # create an instance of ImageDataGenerator class
+    datagen = ImageDataGenerator(
+        rotation_range=20,  # rotation range in degrees
+        width_shift_range=0.2,  # width shift range
+        height_shift_range=0.2,  # height shift range
+        shear_range=0.2,  # shear range in degrees
+        zoom_range=0.2,  # zoom range
+        horizontal_flip=True,  # horizontal flip
+        vertical_flip=False,  # vertical flip
+        fill_mode='nearest'  # filling mode for newly created pixels
+    )
+
+    # fit the datagen on x_train
+    datagen.fit(x_train)
+
+    return datagen
+
+augmented_data = augment_data(x_train)
+generated_images = augmented_data.flow(x_train, batch_size=32)
+
 
 '''
 input_folder = "H:\Python\Repozytoria\Team_work_trees_clasification\df\Original\Pine"
@@ -43,8 +69,3 @@ for filename in os.listdir(input_folder):
 
 print("Image resizing and saving complete.")
 '''
-
-# Image agumentation function
-
-
-# Image proprocessing function
